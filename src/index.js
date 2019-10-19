01ab66592bba9a7e130bbbe6b5c9d2a1fc677e51
Luiz Lopes <luiz@wickeddeveloper.com>
2019-10-18 20:21:47 -0600

    Remove index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
