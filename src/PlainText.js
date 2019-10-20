a228cad4016e4a132cc89ce643866644b5ba7b18
Luiz Lopes <luiz@wickeddeveloper.com>
2019-10-19 21:53:25 -0600

    Refactor: Create PlainText Component

---
+++ src/PlainText.js
@@ -0,0 +1,22 @@
+/* @flow */
+import * as React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+type Props = {
+    value: string,
+    onChange: <T>(SyntheticInputEvent<HTMLSelectElement>) => void
+}
+
+const shouldUpdate = (prevProps: Props, nextProps: Props) : bool => prevProps.value === nextProps.value;
+
+const PlainText = ({value, onChange}: Props) => (
+    <TextField
+        id="plain"
+        name="plain"
+        label="Plain Text"
+        value={value}
+        onChange={onChange}
+        fullWidth />
+);
+
+export default React.memo<Props>(PlainText, shouldUpdate)
