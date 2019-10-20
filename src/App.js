cdc8e8d52f4a3a3f33fafedc4c0cb43895ecbabd
Luiz Lopes <luiz@wickeddeveloper.com>
2019-10-19 21:46:04 -0600

    Refactor: Use Shift Component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import Shift from './Shift';
 
 const { useState, useEffect } = React;
 
@@ -55,8 +51,6 @@ const upper = (delta: number) => (letter: string) => String.fromCharCode((letter
 const lower = (delta: number) => (letter: string) => String.fromCharCode((letter.charCodeAt(0) + delta - 97) % 26 + 97);
 const encrypt = (text: string, delta: number) => text.replace(/[A-Z]/g, upper(delta)).replace(/[a-z]/g, lower(delta));
 
-const shiftValues = [...Array(24).keys()];
-
 function App(): React.Node {
     const classes = useStyles();
     const [text: string, setText] = useState('');
@@ -94,23 +88,7 @@ function App(): React.Node {
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Paper className={classes.delta} elevation={0}>
-                        <FormControl>
-                            <InputLabel htmlFor="delta">Delta</InputLabel>
-                            <Select
-                                id="delta"
-                                name="delta"
-                                value={delta}
-                                onChange={handleDeltaChange}
-                            >
-                                <MenuItem value={0}>
-                                    <em>None</em>
-                                </MenuItem>
-                                {shiftValues.map((idx) => (
-                                    <MenuItem key={idx} value={idx + 1}>{idx + 1}</MenuItem>
-                                ))}
-                            </Select>
-                            <FormHelperText>How many letters to shift.</FormHelperText>
-                        </FormControl>
+                        <Shift value={delta} onChange={handleDeltaChange}/>
                     </Paper>
                 </Grid>
                 <Grid item xs={12} sm={6}>
