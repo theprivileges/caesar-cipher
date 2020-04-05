f71718bd5e234c29002ecab0b5ca199e9c6f2277
Luiz Lopes <luiz@wickeddeveloper.com>
2020-04-05 13:20:27 -0600

    Test: Add tests for encryp util
    Adds tests for both encrypt and decrypt

--- src/utils/index.spec.js
+++ src/utils/index.spec.js
@@ -1,22 +1,23 @@
 import { encrypt } from './index'
 
 describe('utils', () => {
-
+    const shift = 5
+    const unshift = (26 - shift) % 26 // decrypt works in reverse
     describe('encrypt', () => {
         it('should encrypt a message', () => {
-            expect(encrypt('hello', 5)).toEqual('mjqqt')
+            expect(encrypt('hello', shift)).toEqual('mjqqt')
         })
         it('should handle UPPERCASE messages', () => {
-            expect(encrypt('HELLO', 5)).toEqual('MJQQT')
+            expect(encrypt('HELLO', shift)).toEqual('MJQQT')
         })
         it('should handle mixedCase messages', () => {
-            expect(encrypt('Hello', 5)).toEqual('Mjqqt')
+            expect(encrypt('Hello', shift)).toEqual('Mjqqt')
         })
         it('shold decrypt a message', () => {
-            expect(encrypt('mjqqt', 5)).toEqual('hello')
+            expect(encrypt('mjqqt', unshift)).toEqual('hello')
         })
         it('should decrypt a mixedCase message', () => {
-            expect(encrypt('Mjqqt', 5)).toEqual('Hello')
+            expect(encrypt('Mjqqt', unshift)).toEqual('Hello')
         })
     })
 })
